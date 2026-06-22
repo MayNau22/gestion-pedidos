f2c8694d4550f78d2796d9af1013a6d699e307f5
Pame <Pame@Mayra>
2026-06-21 22:47:54 -0500

    Cambios

--- src/main/java/com/uisrael/pedidos2026/dominio/entidades/Test.java
+++ src/main/java/com/uisrael/pedidos2026/dominio/entidades/Test.java
@@ -2,23 +2,23 @@ package com.uisrael.pedidos2026.dominio.entidades;
 
 public class Test {
 	//comentario de mayra
-<<<<<<< HEAD
+
 
 	//comentario del pablo
 
 	//comentario erik holass
 
-=======
+
 	//comentario erik clavijo test3
->>>>>>> branch 'master' of https://github.com/MayNau22/gestion-pedidos.git
+
 	//nuevo comentario
-<<<<<<< HEAD
+
 	//hola pablo crespo
 
-=======
+
 	
 	// nueva modificacion
 	
 	//linea nueva
->>>>>>> branch 'master' of https://github.com/MayNau22/gestion-pedidos.git
+
 }
