d3a7a57b1803ccc9b7c37703ce6ad21fb58d127f
Mayra Ñaupari <e1726895160@uisrael.edu.ec>
2026-07-05 18:59:48 -0500

    Cambio de nombre de campo de relación

--- src/main/java/com/uisrael/pedidos2026/infraestructura/persistencia/jpa/CarritoEntity.java
+++ src/main/java/com/uisrael/pedidos2026/infraestructura/persistencia/jpa/CarritoEntity.java
@@ -22,6 +22,6 @@ public class CarritoEntity {
 	private int idUsuario;
 	private Date fechaCreacion;
 	
-	@OneToMany(mappedBy = "carrito")
+	@OneToMany(mappedBy = "idCarrito")
 	private List<DetalleCarritoEntity> detalleCarrito = new ArrayList<>();
 }
