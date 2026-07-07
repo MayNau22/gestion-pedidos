5a0233ed367082687f2d33d171cbc582b9dfb9e9
Pablo Crespo <e0104846191@uisrael.edu.ec>
2026-07-06 22:16:03 -0500

    Cambio sprin

--- src/main/java/com/uisrael/pedidos2026/infraestructura/persistencia/jpa/PedidosEntity.java
+++ src/main/java/com/uisrael/pedidos2026/infraestructura/persistencia/jpa/PedidosEntity.java
@@ -2,20 +2,17 @@ package com.uisrael.pedidos2026.infraestructura.persistencia.jpa;
 
 import java.util.ArrayList;
 import java.util.Date;
-
 import java.util.List;
-
-import org.hibernate.annotations.ManyToAny;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
-
 
 @Data
 @Entity
@@ -24,9 +21,7 @@ public class PedidosEntity {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int idPedido; //
-
-	private int idEstado;
+	private int idPedido;
 	private Date fechaPedido;
 	private Date fechaEntrega;
 	private String direccionEntrega;
@@ -43,9 +38,13 @@ public class PedidosEntity {
 	@OneToMany(mappedBy = "fkPedidosComprobantePagoEntity")
 	private List<ComprobantesPagoEntity> comprobantespagoentity=new ArrayList<>();
 	
-	@ManyToAny
+	@ManyToOne
 	@JoinColumn(name = "idUsuario")
 	private UsuariosEntity fkUsuariosPedidoEntity;
 	
-
+	
+	@ManyToOne
+	@JoinColumn(name = "idEstado")
+	private EstadosGeneralesEntity fkEstadosPedidosEntity;
+	
 }
